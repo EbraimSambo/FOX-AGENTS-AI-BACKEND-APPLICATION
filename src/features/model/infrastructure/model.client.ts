d5d8a62bfc70f3c8d810e4282e983b02115427bd
ebraimsambo@gmail.com <ebraimsambo@gmail.com>
2025-09-16 20:12:58 +0100

    fix

--- src/features/model/infrastructure/model.client.ts
+++ src/features/model/infrastructure/model.client.ts
@@ -3,7 +3,7 @@ import { ModelEnum, ModelType } from "../domain/entity/model.entity";
 export const models: Array<ModelType> = [
     {
         baseURL: 'https://generativelanguage.googleapis.com/v1beta/openai/',
-        apiKey: 'AIzaSyCn__Oc_08WjQnAVLIZtji9tJu5ttBuuys',
+        apiKey: 'AIzaSyAviVGc9dPEnVvHBLdLjzm4lAFTde7_bb8',
         model: ModelEnum.GEMINI,
         value: "gemini-2.0-flash"
     }
